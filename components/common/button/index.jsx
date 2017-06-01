fffde4e976f6f3a27f34910f3dc93d4a67f0a8d2
jazozaya <jesus@voltera.io>
2017-06-01 14:56:22 -0400

    Updated button component to handle internal links, external links, and callbacks

--- components/common/button/index.jsx
+++ components/common/button/index.jsx
@@ -7,11 +7,16 @@ import { prefixLink } from 'gatsby-helpers'
 export default class Button extends React.Component {
 
   render() {
-    const { label, url, color, internal } = this.props;
 
+    const { label, url, color, internal, onClick } = this.props;
     const type = `button ${color}`;
-    return (<div className="button-wrapper">
-      {internal ? <Link className={type} to={prefixLink(url)}>{label}</Link> : <a className={type} href={url}>{label}</a>}
-    </div>);
+
+    if (internal) {
+      return <Link className={type} to={prefixLink(url)}>{label}</Link>;
+    }
+    if (onClick) {
+      return <div className={type} onClick={onClick}>{label}</div>;
+    }
+    return <a className={type} href={url}>{label}</a>;
   }
 }
