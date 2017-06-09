8cb850ad614502da194f186c43b5605df3d9a4ad
jazozaya <jesus@voltera.io>
2017-06-09 09:07:55 -0400

    Updated customer quote styilng to be transparent and bullets to look more like buttons

--- components/common/bullet/index.jsx
+++ components/common/bullet/index.jsx
@@ -7,13 +7,11 @@ export default class Bullet extends React.Component {
 
   render(){
     const { name, highlight, iconSvg, label } = this.props;
-    const wrapperName = highlight ? "bullet-wrapper highlight" : "bullet-wrapper"
+    const wrapperName = highlight ? "bullet highlight" : "bullet"
     return(
-      <div className="bullet">
         <div onClick={() => this.props.handler(name)} className={wrapperName}>
           <SVGInline svg={iconSvg} />
           <p>{label}</p>
-        </div>
-      </div>);
+        </div>);
     }
   }
