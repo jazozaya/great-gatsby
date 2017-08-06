ab9da2d8a27bb7c3c4e47020d1c9eae89b3732f7
Jesus Zozaya <jesus@voltera.io>
2017-08-06 08:35:35 -0400

    Made specs page work in mobile

--- src/components/technology/specs/row.jsx
+++ src/components/technology/specs/row.jsx
@@ -6,13 +6,16 @@ export default class Row extends React.Component {
 
     // The row may be highlighted dark or light (we alternate)
     // The fist item (category) needs to have a class applied
-    const { light } = this.props;
-    const className = light ? "row light" : "row"
+    const { dark, units } = this.props;
+    const className = dark ? "row dark" : "row"
     const category = React.cloneElement(this.props.children[0], {className:"category"})
-    return (<div className={className}>
-      {category}
-      {this.props.children[1]}
-      {this.props.children[2]}
-    </div>);
+
+    return (
+      <div className={className}>
+        {category}
+        {units === "both" || units === "Metric" ? this.props.children[1]: null}
+        {units === "both" || units === "Imperial" ? this.props.children[2]: null}
+      </div>
+    );
   }
 }
