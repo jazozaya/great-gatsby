9b890c794921f4a2362f441251de231f50b9bced
Jesus Zozaya <jesus@voltera.io>
2017-08-17 19:20:07 -0400

    minor jsx syntax error

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -30,17 +30,17 @@ module.exports = React.createClass({
           <meta name="keywords" content="pcb, printer, circuit, board, 3d, factory" />
           <meta name="author" content="Jesus Zozaya" />
 
-          <meta name="twitter:card" content="summary">
-          <meta name="twitter:site" content="@voltera_io">
+          <meta name="twitter:card" content="summary" />
+          <meta name="twitter:site" content="@voltera_io" />
 
-          <meta property="og:url" content="https://voltera.io">
-          <meta property="og:type" content="website">
-          <meta property="og:title" content="Voltera | Build Hardware Faster">
-          <meta property="og:image" content="https://voltera.io/v-one-box-min.png">
-          <meta property="og:description" content="Prototype on your desktop with the Voltera V-One PCB Printer">
-          <meta property="og:site_name" content="Voltera V-One. Prototyping tool.">
-          <meta property="og:locale" content="en_US">
-          <meta property="article:author" content="">
+          <meta property="og:url" content="https://voltera.io" />
+          <meta property="og:type" content="website" />
+          <meta property="og:title" content="Voltera | Build Hardware Faster" />
+          <meta property="og:image" content="https://voltera.io/v-one-box-min.png" />
+          <meta property="og:description" content="Prototype on your desktop with the Voltera V-One PCB Printer" />
+          <meta property="og:site_name" content="Voltera V-One. Prototyping tool." />
+          <meta property="og:locale" content="en_US" />
+          <meta property="article:author" content="" />
 
           <link rel="shortcut icon" href={favicon} />
           <link href="https://fonts.googleapis.com/css?family=Raleway:600" rel="stylesheet"/>
