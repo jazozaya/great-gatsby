a944edac21406667756c1fcdc9b47f53effc3875
Jesus Zozaya <jesus@voltera.io>
2017-08-22 13:16:14 -0400

    404 page is a thing now

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,11 @@
 import React from 'react'
 
 const NotFoundPage = () => (
-  <div>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </div>
+  <section className="not-found">
+    <h1>Uh Oh! 404 Page not found!</h1>
+    <p>You've requested a page that does not exist anymore.</p>
+    <p>We recently updated our website and it's possible we broke some things along the way. Please let us know what you are looking for by emailing <strong>support@voltera.io</strong>.</p>
+  </section>
 )
 
 export default NotFoundPage
