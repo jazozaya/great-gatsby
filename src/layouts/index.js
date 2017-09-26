848acff687eec219b3454a88fa66cb8c9c369229
Jesus Zozaya <jesus@voltera.io>
2017-09-26 16:59:51 -0400

    testing heap in wip

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -52,6 +52,10 @@ module.exports = React.createClass({
     }
 
 
+    window.heap=window.heap||[],heap.load=function(e,t){window.heap.appid=e,window.heap.config=t=t||{};var r=t.forceSSL||"https:"===document.location.protocol,a=document.createElement("script");a.type="text/javascript",a.async=!0,a.src=(r?"https:":"http:")+"//cdn.heapanalytics.com/js/heap-"+e+".js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(a,n);for(var o=function(e){return function(){heap.push([e].concat(Array.prototype.slice.call(arguments,0)))}},p=["addEventProperties","addUserProperties","clearEventProperties","identify","removeEventProperty","setEventProperties","track","unsetEventProperty"],c=0;c<p.length;c++)heap[p[c]]=o(p[c])};
+    heap.load("2791463426");
+
+
   },
 
   render() {
@@ -79,7 +83,6 @@ module.exports = React.createClass({
 
           <link rel="shortcut icon" href={favicon} />
           <script defer="defer" type="text/javascript" src="https://www.googleadservices.com/pagead/conversion_async.js" charset="utf-8"></script>
-
         </Helmet>
         <Header pageName={this.props.location.pathname} />
         <DummyHeader />
