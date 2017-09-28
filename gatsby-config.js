16a866380c797008032204259175bd322ea0a4d1
Jesus Zozaya <jesus@voltera.io>
2017-09-28 15:48:48 -0400

    disabled google analytics, added hotjar only on desktop and tablet

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,13 +4,13 @@ module.exports = {
   },
   plugins: [
             `gatsby-plugin-react-helmet`,
-            `gatsby-plugin-sass`,
-            {
-              resolve: `gatsby-plugin-google-analytics`,
-              options: {
-                trackingId: 'UA-41924051-3',
-              }
-            }
+            `gatsby-plugin-sass`
+            // {
+            //   resolve: `gatsby-plugin-google-analytics`,
+            //   options: {
+            //     trackingId: 'UA-41924051-3',
+            //   }
+            // }
           ],
   pathPrefix: '/'
 }
--- src/layouts/api.js
+++ src/layouts/api.js
@@ -1,3 +1,5 @@
+import Bowser from 'bowser'
+
 export function loadIntercom() {
 
   var w = window;
@@ -33,13 +35,16 @@ export function loadIntercom() {
 }
 
 export function loadHeap() {
-
   window.heap=window.heap||[],heap.load=function(e,t){window.heap.appid=e,window.heap.config=t=t||{};var r=t.forceSSL||"https:"===document.location.protocol,a=document.createElement("script");a.type="text/javascript",a.async=!0,a.src=(r?"https:":"http:")+"//cdn.heapanalytics.com/js/heap-"+e+".js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(a,n);for(var o=function(e){return function(){heap.push([e].concat(Array.prototype.slice.call(arguments,0)))}},p=["addEventProperties","addUserProperties","clearEventProperties","identify","removeEventProperty","setEventProperties","track","unsetEventProperty"],c=0;c<p.length;c++)heap[p[c]]=o(p[c])};
         heap.load("3887165686");
-
 }
 
 export function loadHotjar() {
+
+  if (Bowser.mobile){
+    return;
+  }
+
   (function(h,o,t,j,a,r){
       h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
       h._hjSettings={hjid:640069,hjsv:5};
