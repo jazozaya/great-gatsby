e037e47dde69d45246dfcd071d76582aaf2250a9
Jesus Zozaya <jesus@voltera.io>
2017-10-23 13:42:19 -0400

    Made search bar narrow in mobile, fixed issue where store headers were not highlighting properly, swapped order of intouch components

--- src/components/common/header/desktop.jsx
+++ src/components/common/header/desktop.jsx
@@ -13,8 +13,13 @@ import { collections as c } from 'components/store/constants'
 export default class Desktop extends React.Component {
 
   // Small class that determines if we should highlight link.
-  getClass(currentPage, pageName) {
-    if(currentPage.indexOf(pageName) === 0) { //IE does not support includes...
+  startsWith(currentPage, query) {
+    if(currentPage.indexOf(query) === 0){
+      return "selected"
+    }
+  }
+  includes(currentPage, query) {
+    if(currentPage.indexOf(query) !== -1){
       return "selected"
     }
   }
@@ -23,11 +28,11 @@ export default class Desktop extends React.Component {
     return (
       <div className="child-wrapper">
         <div className="child-header">
-          <Link className={this.getClass(pageName, urls.technology)} to={urls.technology}>How it works</Link>
-          <Link className={this.getClass(pageName, urls.print )} to={urls.print}>Circuit Printing </Link>
-          <Link className={this.getClass(pageName, urls.paste)} to={urls.paste}>Paste Dispensing</Link>
-          <Link className={this.getClass(pageName, urls.software)} to={urls.software}>Desktop Software</Link>
-          <Link className={this.getClass(pageName, urls.experiment)} to={urls.experiment}>Experiment</Link>
+          <Link className={this.startsWith(pageName, urls.technology)} to={urls.technology}>How it works</Link>
+          <Link className={this.startsWith(pageName, urls.print )} to={urls.print}>Circuit Printing </Link>
+          <Link className={this.startsWith(pageName, urls.paste)} to={urls.paste}>Paste Dispensing</Link>
+          <Link className={this.startsWith(pageName, urls.software)} to={urls.software}>Desktop Software</Link>
+          <Link className={this.startsWith(pageName, urls.experiment)} to={urls.experiment}>Experiment</Link>
         </div>
       </div>
     )
@@ -38,14 +43,14 @@ export default class Desktop extends React.Component {
       <div className="child-wrapper">
         <div className="child-header">
           <Link className={pageName === "/store/" ? "selected" : null} to={'/store/'}>{c.vOne.title}</Link>
-          <Link className={this.getClass(pageName, c.bundles.handle)} to={`/store/collection/${c.bundles.handle}/`}>{c.bundles.title}</Link>
-          <Link className={this.getClass(pageName, c.accessories.handle)} to={`/store/collection/${c.accessories.handle}/`}>{c.accessories.title}</Link>
-          <Link className={this.getClass(pageName, c.inks.handle)} to={`/store/collection/${c.inks.handle}/`}>{c.inks.title}</Link>
-          <Link className={this.getClass(pageName, c.solder.handle)} to={`/store/collection/${c.solder.handle}/`}>{c.solder.title}</Link>
-          <Link className={this.getClass(pageName, c.substrates.handle)} to={`/store/collection/${c.substrates.handle}/`}>{c.substrates.title}</Link>
-          <Link className={this.getClass(pageName, c.swag.handle)} to={`/store/collection/${c.swag.handle}/`}>{c.swag.title}</Link>
+          <Link className={this.includes(pageName, c.bundles.handle)} to={`/store/collection/${c.bundles.handle}/`}>{c.bundles.title}</Link>
+          <Link className={this.includes(pageName, c.accessories.handle)} to={`/store/collection/${c.accessories.handle}/`}>{c.accessories.title}</Link>
+          <Link className={this.includes(pageName, c.inks.handle)} to={`/store/collection/${c.inks.handle}/`}>{c.inks.title}</Link>
+          <Link className={this.includes(pageName, c.solder.handle)} to={`/store/collection/${c.solder.handle}/`}>{c.solder.title}</Link>
+          <Link className={this.includes(pageName, c.substrates.handle)} to={`/store/collection/${c.substrates.handle}/`}>{c.substrates.title}</Link>
+          <Link className={this.includes(pageName, c.swag.handle)} to={`/store/collection/${c.swag.handle}/`}>{c.swag.title}</Link>
           <div className="divider"></div>
-          <Link className={this.getClass(pageName, 'cart')} to={`/store/cart/`}><SVGInline className="cart-icon" svg={rawSVG} />Cart</Link>
+          <Link className={this.includes(pageName, 'cart')} to={`/store/cart/`}><SVGInline className="cart-icon" svg={rawSVG} />Cart</Link>
         </div>
       </div>
     )
@@ -72,12 +77,12 @@ export default class Desktop extends React.Component {
           <div className="parent-header">
             <Logo />
             <div className="links">
-              <Link className={this.getClass(pageName, "/faq/")} to="/faq/">FAQ</Link>
-              <Link className={this.getClass(pageName, "/product/")} to={urls.technology}>Product</Link>
+              <Link className={this.startsWith(pageName, "/faq/")} to="/faq/">FAQ</Link>
+              <Link className={this.startsWith(pageName, "/product/")} to={urls.technology}>Product</Link>
               <a href="http://community.voltera.io">Forums</a>
               <a href="http://support.voltera.io">Support</a>
-              <Link className={this.getClass(pageName, "/contact/")}to="/contact/">Contact</Link>
-              <Link className={`store-link ${this.getClass(pageName,"/store/")}`} to="/store/">Store</Link>
+              <Link className={this.startsWith(pageName, "/contact/")}to="/contact/">Contact</Link>
+              <Link className={`store-link ${this.startsWith(pageName,"/store/")}`} to="/store/">Store</Link>
             </div>
           </div>
         </div>
--- src/components/contact/inTouch/index.jsx
+++ src/components/contact/inTouch/index.jsx
@@ -1,37 +1,27 @@
 import React from 'react'
-
 import Button from 'components/common/button'
 
 import './intouch.scss'
 
-//import chatIcon from './chat.min.svg'
 import callIcon from './call.min.svg'
 import quoteIcon from './quote.min.svg'
 
-
-// <div className="action">
-//   <img src={chatIcon} />
-//   <p>Start a live conversation with us and get answers right away.</p>
-//   <div className="button-wrap">
-//     <Button label="Live Chat" url="/request/chat/" internal={true} color="dark"/>
-//   </div>
-// </div>
 export default class InTouch extends React.Component {
   render() {
     return(
       <div className="in-touch flex-row">
         <div className="action">
-          <img src={callIcon} />
-          <p>Find a time that works for you and we'll call you back!</p>
-          <div className="button-wrap">
-            <Button label="Book a Call" url="/request/call/" internal={true} color="dark"/>
-          </div>
-        </div>
-        <div className="action">
           <img src={quoteIcon} />
           <p>Tell us your contact and shipping information and we'll send you a quote!</p>
           <div className="button-wrap">
             <Button label="Get a Quote" url="/request/quote/" internal={true} color="dark"/>
+          </div>
+        </div>
+        <div className="action">
+          <img src={callIcon} />
+          <p>Find a time that works for you and we'll call you back!</p>
+          <div className="button-wrap">
+            <Button label="Book a Call" url="/request/call/" internal={true} color="dark"/>
           </div>
         </div>
       </div>
