46d3153b2589b51364d2b130d94090792413c1b8
Jesus Zozaya <jesus@voltera.io>
2018-12-06 11:22:36 -0500

    updated warranty description in the FAQ

--- src/components/faq/something.jsx
+++ src/components/faq/something.jsx
@@ -12,7 +12,7 @@ export default class SomethingFAQ extends React.Component {
     return (
       <Template id={id} title={title} all={all}>
         <Question underline title="What is your warranty policy?">
-          <p>We offer a 6 month warranty from date of delivery.  More information can be found in our <Link to="/docs/tos">Terms of Service</Link>.</p>
+          <p>We offer a 1 year warranty from date of delivery.  More information can be found in our <Link to="/legal/tos/">Terms of Service</Link>.</p>
         </Question>
         <Question underline title="Do you offer bulk discounts?">
           <p>Yes. Contact <strong>sales@voltera.io</strong>.</p>
