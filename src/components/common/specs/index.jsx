4d4ff3b9a473815a92c9fb5b7a7aaab387af4f47
Jesus Zozaya <jesus@voltera.io>
2019-09-19 10:12:45 -0400

    corrected area in specs

--- src/components/common/specs/index.jsx
+++ src/components/common/specs/index.jsx
@@ -158,8 +158,8 @@ export default class Specs extends React.Component {
             <h3>Print Bed</h3>
             <Row units={units}>
               <p>Print Area</p>
-              <p>	128mm x 105mm</p>
-              <p>5.0” x 4.1”</p>
+              <p>	128mm x 116mm</p>
+              <p>5.0” x 4.5”</p>
             </Row>
             <Row units={units} dark>
               <p>Max. heated bed temperature</p>
