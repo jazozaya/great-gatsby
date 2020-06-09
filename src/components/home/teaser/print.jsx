c2bfcb3e1a8057c308c084cc9888f89eb300c58c
Jesus Zozaya <jesus@voltera.io>
2020-06-09 16:09:07 -0400

    Updated video link and flexible conductive datasheet

--- src/components/home/teaser/print.jsx
+++ src/components/home/teaser/print.jsx
@@ -1,11 +1,11 @@
-import React from "react";
-import YouTube from "components/common/youtube";
+import React from 'react';
+import YouTube from 'components/common/youtube';
 
-import Button from "components/common/button";
+import Button from 'components/common/button';
 
-import { StaticQuery, graphql } from "gatsby";
+import { StaticQuery, graphql } from 'gatsby';
 
-import styles from "./teaser.module.scss";
+import styles from './teaser.module.scss';
 
 export default () => (
   <StaticQuery
@@ -20,26 +20,30 @@ export default () => (
         }
       }
     `}
-    render={data => <Feature data={data} />}
+    render={(data) => <Feature data={data} />}
   />
 );
 
 class Feature extends React.Component {
   render() {
     return (
-      <div className="flex-row">
+      <div className='flex-row'>
         <div className={styles.videoWrap}>
-          <YouTube videoId="PeW1nURJ5ww" fluid={this.props.data.file.childImageSharp.fluid} width="600" />
+          <YouTube videoId='mOzt_sbODZg' fluid={this.props.data.file.childImageSharp.fluid} width='600' />
         </div>
         <div className={styles.description}>
           <h2>Print your own circuit boards.</h2>
-          <p>On your desktop and on your schedule, the Voltera V-One gets you from prototype to production faster than ever before.</p>
           <p>
-            Create <strong>two layer circuit boards</strong> on your desk. Load your Gerber files and watch the dispenser lay down a{" "}
-            <strong>silver-based conductive ink</strong> to print your circuit right before your eyes.
+            On your desktop and on your schedule, the Voltera V-One gets you from prototype to production faster than
+            ever before.
+          </p>
+          <p>
+            Create <strong>two layer circuit boards</strong> on your desk. Load your Gerber files and watch the
+            dispenser lay down a <strong>silver-based conductive ink</strong> to print your circuit right before your
+            eyes.
           </p>
           <div className={styles.buttonWrap}>
-            <Button label="Print Circuits" internal url="/product/print/" color="dark" />
+            <Button label='Print Circuits' internal url='/product/print/' color='dark' />
           </div>
         </div>
       </div>
